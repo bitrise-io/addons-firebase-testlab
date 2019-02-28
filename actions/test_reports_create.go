6670ad79a3657d8231a6c15167538b0aae483542
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-02-28 16:12:30 +0100

    Change column type

--- actions/test_reports_create.go
+++ actions/test_reports_create.go
@@ -48,10 +48,16 @@ func TestReportsPostHandler(c buffalo.Context) error {
 		return c.Render(http.StatusBadRequest, r.JSON(map[string]string{"error": "Failed to decode test report data"}))
 	}
 
+	stepInfo, err := json.Marshal(params.Step)
+	if err != nil {
+		log.Errorf("Failed to marshal step info, error: %+v", errors.WithStack(err))
+		return c.Render(http.StatusInternalServerError, r.JSON(map[string]string{"error": "Internal error"}))
+	}
+
 	testReport := &models.TestReport{
 		Filename:  params.Filename,
 		Filesize:  params.Filesize,
-		Step:      params.Step,
+		Step:      stepInfo,
 		Uploaded:  false,
 		AppSlug:   appSlug,
 		BuildSlug: buildSlug,
