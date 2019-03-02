922ab436a11582ee378fbc9635d6a46a5877cd82
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-03-02 15:15:38 +0100

    Simplify model file

--- models/test_report_asset.go
+++ models/test_report_asset.go
@@ -1,48 +1,21 @@
 package models
 
 import (
-	"encoding/json"
+	"time"
+
 	"github.com/gobuffalo/pop"
 	"github.com/gobuffalo/uuid"
 	"github.com/gobuffalo/validate"
-	"time"
 )
 
+// TestReportAsset ...
 type TestReportAsset struct {
 	ID        uuid.UUID `json:"id" db:"id"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
 
-// String is not required by pop and may be deleted
-func (t TestReportAsset) String() string {
-	jt, _ := json.Marshal(t)
-	return string(jt)
-}
-
-// TestReportAssets is not required by pop and may be deleted
-type TestReportAssets []TestReportAsset
-
-// String is not required by pop and may be deleted
-func (t TestReportAssets) String() string {
-	jt, _ := json.Marshal(t)
-	return string(jt)
-}
-
-// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
-// This method is not required and may be deleted.
+// Validate ...
 func (t *TestReportAsset) Validate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
-}
-
-// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
-// This method is not required and may be deleted.
-func (t *TestReportAsset) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
-}
-
-// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
-// This method is not required and may be deleted.
-func (t *TestReportAsset) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
 	return validate.NewErrors(), nil
 }
