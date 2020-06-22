e1366c9e5ef6ba552a7858667d165bf71993fe88
Gergely Bekesi <gergely.bekesi@bitrise.io>
2020-06-22 13:42:50 +0200

    Add analytics event methods

--- analytics/client.go
+++ analytics/client.go
@@ -7,11 +7,31 @@ import (
 
 	"github.com/gobuffalo/uuid"
 	"go.uber.org/zap"
+	"google.golang.org/api/testing/v1"
 
 	segment "gopkg.in/segmentio/analytics-go.v3"
 )
 
 var client segment.Client
+
+const (
+	eventAddonProvisioned   = "vdt_android_addon_provisioned"
+	eventAddonDeprovisioned = "vdt_android_addon_deprovisioned"
+	eventAddonPlanChanged   = "vdt_android_addon_plan_changed"
+	eventAddonSSOLogin      = "vdt_android_addon_sso_login"
+
+	eventUploadFileUploadRequested = "vdt_android_addon_file_upload_requested"
+
+	eventTestingTestStarted          = "vdt_android_addon_test_started"
+	eventTestingTestFinished         = "vdt_android_addon_test_finished"
+	eventTestingTestStartedOnDevice  = "vdt_android_addon_test_started_on_device"
+	eventTestingTestFinishedOnDevice = "vdt_android_addon_test_finished_on_device"
+
+	eventIOSTestingTestStarted          = "vdt_ios_addon_test_started"
+	eventIOSTestingTestFinished         = "vdt_ios_addon_test_finished"
+	eventIOSTestingTestStartedOnDevice  = "vdt_ios_addon_test_started_on_device"
+	eventIOSTestingTestFinishedOnDevice = "vdt_ios_addon_test_finished_on_device"
+)
 
 // Client ...
 type Client struct {
@@ -85,6 +105,200 @@ func (c *Client) NumberOfTestReports(appSlug, buildSlug string, count int, time 
 	})
 	if err != nil {
 		c.logger.Warn("Failed to track analytics (NumberOfTestReports)", zap.Error(err))
+	}
+}
+
+// SendTestStartedEvent ...
+func (c *Client) SendTestStartedEvent(appSlug, buildSlug, testType string, eventProperties map[string]interface{}) {
+	c.sendTestingEvent(eventTestingTestStarted, appSlug, buildSlug, testType, eventProperties)
+}
+
+// SendTestFinishedEvent ...
+func (c *Client) SendTestFinishedEvent(appSlug, buildSlug, testType string, eventProperties map[string]interface{}) {
+	c.sendTestingEvent(eventTestingTestFinished, appSlug, buildSlug, testType, eventProperties)
+}
+
+// SendTestStartedOnDeviceEvent ...
+func (c *Client) SendTestStartedOnDeviceEvent(appSlug, buildSlug, testType string, devices []*testing.AndroidDevice, eventProperties map[string]interface{}) {
+	c.sendTestingEventDevices(eventTestingTestStartedOnDevice, appSlug, buildSlug, testType, devices, eventProperties)
+}
+
+// SendTestFinishedOnDeviceEvent ...
+func (c *Client) SendTestFinishedOnDeviceEvent(appSlug, buildSlug, testType string, devices []*testing.AndroidDevice, eventProperties map[string]interface{}) {
+	c.sendTestingEventDevices(eventTestingTestFinishedOnDevice, appSlug, buildSlug, testType, devices, eventProperties)
+}
+
+// SendIOSTestStartedOnDeviceEvent ...
+func (c *Client) SendIOSTestStartedOnDeviceEvent(appSlug, buildSlug, testType string, devices []*testing.IosDevice, eventProperties map[string]interface{}) {
+	c.sendIOSTestingEventDevices(eventIOSTestingTestStartedOnDevice, appSlug, buildSlug, testType, devices, eventProperties)
+}
+
+// SendIOSTestFinishedOnDeviceEvent ...
+func (c *Client) SendIOSTestFinishedOnDeviceEvent(appSlug, buildSlug, testType string, devices []*testing.IosDevice, eventProperties map[string]interface{}) {
+	c.sendIOSTestingEventDevices(eventIOSTestingTestFinishedOnDevice, appSlug, buildSlug, testType, devices, eventProperties)
+}
+
+// SendAddonProvisionedEvent ...
+func (c *Client) SendAddonProvisionedEvent(appSlug, currentPlan, newPlan string) {
+	c.sendAddonEvent(eventAddonProvisioned, appSlug, currentPlan, newPlan)
+}
+
+// SendAddonDeprovisionedEvent ...
+func (c *Client) SendAddonDeprovisionedEvent(appSlug, currentPlan, newPlan string) {
+	c.sendAddonEvent(eventAddonDeprovisioned, appSlug, currentPlan, newPlan)
+}
+
+// SendAddonPlanChangedEvent ...
+func (c *Client) SendAddonPlanChangedEvent(appSlug, currentPlan, newPlan string) {
+	c.sendAddonEvent(eventAddonPlanChanged, appSlug, currentPlan, newPlan)
+}
+
+// SendAddonSSOLoginEvent ...
+func (c *Client) SendAddonSSOLoginEvent(appSlug, currentPlan, newPlan string) {
+	c.sendAddonEvent(eventAddonSSOLogin, appSlug, currentPlan, newPlan)
+}
+
+// SendUploadRequestedEvent ...
+func (c *Client) SendUploadRequestedEvent(event, appSlug, buildSlug string) {
+	if c.client == nil {
+		return
+	}
+	err := c.client.Enqueue(segment.Track{
+		UserId: appSlug,
+		Event:  eventUploadFileUploadRequested,
+		Properties: segment.NewProperties().
+			Set("app_slug", appSlug).
+			Set("build_slug", buildSlug),
+		Timestamp: time.Now(),
+	})
+	if err != nil {
+		c.logger.Warn("Failed to track analytics (SendUploadRequestedEvent)", zap.Error(err))
+	}
+}
+
+func (c *Client) sendTestingEvent(event, appSlug, buildSlug, testType string, eventProperties map[string]interface{}) {
+	if c.client == nil {
+		return
+	}
+
+	trackProps := segment.NewProperties().
+		Set("app_slug", appSlug).
+		Set("build_slug", buildSlug)
+
+	if testType != "" {
+		trackProps = trackProps.Set("test_type", testType)
+	}
+	if eventProperties != nil {
+		for key, value := range eventProperties {
+			trackProps = trackProps.Set(key, value)
+		}
+	}
+	err := c.client.Enqueue(segment.Track{
+		UserId:     appSlug,
+		Event:      event,
+		Properties: trackProps,
+		Timestamp:  time.Now(),
+	})
+
+	if err != nil {
+		c.logger.Warn("Failed to track analytics (sendTestingEvent)", zap.String("event", event), zap.Error(err))
+	}
+}
+
+func (c *Client) sendTestingEventDevices(event, appSlug, buildSlug, testType string, devices []*testing.AndroidDevice, eventProperties map[string]interface{}) {
+	if c.client == nil {
+		return
+	}
+
+	for _, device := range devices {
+		trackProps := segment.NewProperties().
+			Set("app_slug", appSlug).
+			Set("build_slug", buildSlug)
+
+		if testType != "" {
+			trackProps = trackProps.Set("test_type", testType)
+		}
+		if eventProperties != nil {
+			for key, value := range eventProperties {
+				trackProps = trackProps.Set(key, value)
+			}
+		}
+		trackProps = trackProps.Set("device_id", device.AndroidModelId)
+		trackProps = trackProps.Set("device_os_version", device.AndroidVersionId)
+		trackProps = trackProps.Set("device_language", device.Locale)
+		trackProps = trackProps.Set("device_orientation", device.Orientation)
+
+		err := c.client.Enqueue(segment.Track{
+			UserId:     appSlug,
+			Event:      event,
+			Properties: trackProps,
+			Timestamp:  time.Now(),
+		})
+
+		if err != nil {
+			c.logger.Warn("Failed to track analytics (sendTestingEventDevices)", zap.String("event", event), zap.Error(err))
+		}
+	}
+}
+
+func (c *Client) sendIOSTestingEventDevices(event, appSlug, buildSlug, testType string, devices []*testing.IosDevice, eventProperties map[string]interface{}) {
+	if c.client == nil {
+		return
+	}
+
+	for _, device := range devices {
+		trackProps := segment.NewProperties().
+			Set("app_slug", appSlug).
+			Set("build_slug", buildSlug)
+
+		if testType != "" {
+			trackProps = trackProps.Set("test_type", testType)
+		}
+		if eventProperties != nil {
+			for key, value := range eventProperties {
+				trackProps = trackProps.Set(key, value)
+			}
+		}
+		trackProps = trackProps.Set("device_id", device.IosModelId)
+		trackProps = trackProps.Set("device_os_version", device.IosVersionId)
+		trackProps = trackProps.Set("device_language", device.Locale)
+		trackProps = trackProps.Set("device_orientation", device.Orientation)
+
+		err := c.client.Enqueue(segment.Track{
+			UserId:     appSlug,
+			Event:      event,
+			Properties: trackProps,
+			Timestamp:  time.Now(),
+		})
+
+		if err != nil {
+			c.logger.Warn("Failed to track analytics (sendIOSTestingEventDevices)", zap.String("event", event), zap.Error(err))
+		}
+	}
+}
+
+func (c *Client) sendAddonEvent(event, appSlug, currentPlan, newPlan string) {
+	if c.client == nil {
+		return
+	}
+
+	trackProps := segment.NewProperties().
+		Set("app_slug", appSlug)
+	if currentPlan != "" {
+		trackProps = trackProps.Set("old_plan", currentPlan)
+	}
+	if newPlan != "" {
+		trackProps = trackProps.Set("plan", newPlan)
+	}
+	err := c.client.Enqueue(segment.Track{
+		UserId:     appSlug,
+		Event:      event,
+		Properties: trackProps,
+		Timestamp:  time.Now(),
+	})
+
+	if err != nil {
+		c.logger.Warn("Failed to track analytics (sendTestingEvent)", zap.String("event", event), zap.Error(err))
 	}
 }
 
